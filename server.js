4cf36523b2e348a741c1fda00bf10165a555fb1c
matiastucci <matiastucci@gmail.com>
2014-10-27 23:46:08 -0300

    port fix

--- server.js
+++ server.js
@@ -1,34 +1,36 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var app   = require('express')();
+var http  = require('http').Server(app);
+var io    = require('socket.io')(http);
+
+var port  = process.env.PORT || 8080;
 
 app.get('/', function(req, res){
-  res.send('chat server');
+  res.send('welcome to my chat server');
 });
 
 io.on('connection', function(socket){
 
-	var username = '';
+  var username = '';
 
   socket.on('new user', function(newUsername){
-  	username = newUsername;
+    username = newUsername;
     io.emit('new user', newUsername);
   });
 
   socket.on('chat message', function(msg,username){
-  	var response = {
-  		content: msg,
-  		username:username
-  	};
+    var response = {
+      content: msg,
+      username:username
+    };
     io.emit('chat message', response);
   });
 
   socket.on('disconnect', function(){
-  	io.emit('user disconnected',username);
+    io.emit('user disconnected',username);
   });
 
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
