50603679d166224b7144476c13fdd610ed80e64b
matiastucci <matiastucci@gmail.com>
2014-11-02 20:49:30 -0300

    start/stop typing event

--- server.js
+++ server.js
@@ -4,31 +4,72 @@ var io    = require('socket.io')(http);
 
 var port  = process.env.PORT || 8080;
 
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET");
+  next();
+});
+
 app.get('/', function(req, res){
   res.send('welcome to my chat server');
 });
 
-io.on('connection', function(socket){
+// usernames which are currently connected to the chat
+var usernames = {};
 
-  var username = '';
+io.on('connection', function (socket) {
+  var addedUser = false;
 
-  socket.on('new user', function(newUsername){
-    username = newUsername;
-    io.emit('new user', newUsername);
+  socket.on('new message', function (data) {
+    socket.broadcast.emit('new message', {
+      username: socket.username,
+      content: data
+    });
   });
 
-  socket.on('chat message', function(msg,username){
-    var response = {
-      content: msg,
-      username:username
-    };
-    io.emit('chat message', response);
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    addedUser = true;
+    // echo globally (all clients) that a person has connected
+    socket.broadcast.emit('user joined', {
+      username: socket.username
+    });
   });
 
-  socket.on('disconnect', function(){
-    io.emit('user disconnected',username);
+  // when the client emits 'typing', we broadcast it to others
+  socket.on('typing', function () {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    });
   });
 
+  // when the client emits 'stop typing', we broadcast it to others
+  socket.on('stop typing', function () {
+    socket.broadcast.emit('stop typing', {
+      username: socket.username
+    });
+  });
+
+  socket.on('disconnect', function () {
+    // remove the username from global usernames list
+    if (addedUser) {
+      delete usernames[socket.username];
+
+      // echo globally that this client has left
+      socket.broadcast.emit('user left', {
+        username: socket.username
+      });
+    }
+  });
+
+});
+
+app.get('/usernames', function(req, res){
+  res.send(usernames);
 });
 
 http.listen(port, function(){
